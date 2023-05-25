f45df9adff76d5918a5014c1bbe6d024c12d967b
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-05-25 19:55:12 -0400

    Changed file/dir names

--- train_adapters.py
+++ train_adapters.py
@@ -2,7 +2,7 @@ import torch
 from transformers import BertTokenizer, BertForMaskedLM, LineByLineTextDataset, DataCollatorForLanguageModeling, TrainingArguments,AdapterTrainer
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-corpus_file = "./data/conceptnet_corpus.txt"
+corpus_file = "./conceptnet_data/conceptnet_corpus.txt"
 
 dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
